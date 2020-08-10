4e900b3a289c230a183ae5317c6fe1dab3a6f87f
ilmerainen <rborukha@gmail.com>
2020-08-10 23:15:24 +0300

    init project

---
+++ frontend/src/containers/App/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Typography} from "@material-ui/core";
+
+function App() {
+    return (
+        <Typography variant="h1">Hello world!</Typography>
+    );
+}
+
+export default App;
